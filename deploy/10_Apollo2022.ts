61e704ebed5fc30dbdbcf510e52a3f2e3f64d077
moonwalker <m00n3r@protonmail.com>
2021-11-22 18:09:48 +0200

    Add setupRelease in apollo2022 deployment script

--- deploy/10_Apollo2022.ts
+++ deploy/10_Apollo2022.ts
@@ -5,9 +5,18 @@ import { config } from "../config";
 import { getErc20Factory } from "../utils";
 import { parseEther } from "ethers/lib/utils";
 
-const func: DeployFunction = async function ({ getNamedAccounts, deployments, network }: HardhatRuntimeEnvironment) {
+const func: DeployFunction = async function ({
+  getNamedAccounts,
+  deployments,
+  network,
+  ethers,
+}: HardhatRuntimeEnvironment) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  const start = time.now() + time.hours(1);
+  const end = start + time.days(3);
+  const releaseAmount = 500;
 
   let wethAddr: string = config.network[network.name]?.WETH?.address;
 
@@ -35,6 +44,15 @@ const func: DeployFunction = async function ({ getNamedAccounts, deployments, ne
     args: [wethAddr],
     log: true,
   });
+
+  const apollo2022 = await ethers.getContract("Apollo2022");
+
+  log(`Deploying Apollo2022(
+    start=${start},
+    end=${end},
+    releaseMaxSupply=${releaseAmount}
+  )`);
+  await apollo2022.setupRelease(start, end, releaseAmount);
 };
 
 export default func;
