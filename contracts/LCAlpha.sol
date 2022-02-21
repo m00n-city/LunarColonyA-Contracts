7b37f4940b5a0fd35927d943d89776d852dccf1d
moonwalker <m00n3r@protonmail.com>
2022-02-21 13:51:07 +0200

    Remove ERC721Enumerable and add totalSupply, bpMint functions

--- contracts/LCAlpha.sol
+++ contracts/LCAlpha.sol
@@ -15,15 +15,16 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
  * @title LunarColonyAlpha contract
  * @dev Extends ERC721 Non-Fungible Token Standard basic implementation
  */
-contract LCAlpha is ERC721, ERC721Enumerable, Ownable {
+contract LCAlpha is ERC721, Ownable {
     enum SaleState {
         Paused,
-        Apollo2022,
+        BoardingPass,
         Open
     }
 
     string public PROVENANCE;
     uint256 public constant PRICE = 0.08 ether;
+    uint256 public constant BP_PRICE = 0.06 ether;
     uint256 public constant MAX_PURCHASE = 20 + 1;
     uint256 public constant MAX_SUPPLY = 10000 + 1;
     uint256 public constant RESERVED_TOKENS = 100;
@@ -32,10 +33,20 @@ contract LCAlpha is ERC721, ERC721Enumerable, Ownable {
     string public baseURI;
     SaleState saleState = SaleState.Paused;
 
-    constructor()
-        ERC721('Lunar Colony Alpha', 'LCA')
-        ERC721Enumerable()
-    {}
+    uint256 public totalSupply;
+    mapping(address => uint256) public bpMintsPerAddr;
+
+    modifier validateEthAmount(uint256 price, uint256 amount) {
+        require(price * amount == msg.value, "Incorrect ETH value sent");
+        _;
+    }
+
+    modifier saleIsActive(SaleState state) {
+        require(saleState == state, "Sale not active");
+        _;
+    }
+
+    constructor() ERC721("Lunar Colony Alpha", "LCA") {}
 
     function withdraw() public onlyOwner {
         uint256 balance = address(this).balance;
@@ -43,10 +54,10 @@ contract LCAlpha is ERC721, ERC721Enumerable, Ownable {
     }
 
     /**
-     * reserve for DAO
+     * @notice reserve for DAO
      */
     function reserveTokens() public onlyOwner {
-        uint256 supply = totalSupply();
+        uint256 supply = totalSupply;
         uint256 i;
         for (i = 0; i < RESERVED_TOKENS; i++) {
             _safeMint(msg.sender, supply + i);
@@ -68,13 +79,6 @@ contract LCAlpha is ERC721, ERC721Enumerable, Ownable {
         return baseURI;
     }
 
-    function saleIsActive() public view returns (bool) {
-        return saleState != SaleState.Paused;
-    }
-
-    /**
-     * @notice Set sale state
-     */
     function setSaleState(SaleState newSaleState) public onlyOwner {
         saleState = newSaleState;
     }
@@ -84,45 +88,68 @@ contract LCAlpha is ERC721, ERC721Enumerable, Ownable {
     }
 
     /**
-     * Mint tokens
+     * @notice Mint for boarding pass holders
      */
-    function mint(uint256 numberOfTokens) public payable {
-        require(saleIsActive(), "Sale must be active");
+    function bpMint(
+        uint256 amount,
+        uint256 allowedAmount,
+        bytes32[] calldata proof
+    ) public payable saleIsActive(SaleState.BoardingPass) validateEthAmount(BP_PRICE, amount) {
         require(
-            numberOfTokens < MAX_PURCHASE,
-            "Max purchase exceeded"
+            MerkleProof.verify(
+                proof,
+                merkleRoot,
+                keccak256(abi.encodePacked(msg.sender, allowedAmount))
+            ),
+            "Invalid Merkle Tree proof supplied"
         );
-        require(
-            totalSupply() + numberOfTokens < MAX_SUPPLY,
-            "Purchase would exceed max supply"
-        );
-        require(
-            PRICE * numberOfTokens == msg.value,
-            "Ether value sent is not correct"
-        );
+        require(bpMintsPerAddr[msg.sender] + amount <= allowedAmount, "Exceeds allowed amount");
 
-        for (uint256 i = 0; i < numberOfTokens; i++) {
-            uint256 mintIndex = totalSupply();
-            _safeMint(msg.sender, mintIndex);
+        bpMintsPerAddr[msg.sender] += amount;
+
+        for (uint256 i = 0; i < amount; i++) {
+            _mintNext();
         }
     }
 
-    /* */
-    function supportsInterface(bytes4 interfaceId)
+    /**
+     * @notice Public mint
+     */
+    function mint(uint256 amount)
         public
-        view
-        virtual
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
+        payable
+        saleIsActive(SaleState.Open)
+        validateEthAmount(BP_PRICE, amount)
     {
-        return super.supportsInterface(interfaceId);
+        require(amount < MAX_PURCHASE, "Max purchase exceeded");
+        require(totalSupply + amount < MAX_SUPPLY, "Purchase would exceed max supply");
+
+        for (uint256 i = 0; i < amount; i++) {
+            _mintNext();
+        }
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal virtual override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
+    function _mintNext() private {
+        _mint(msg.sender, totalSupply);
+        unchecked {
+            totalSupply++;
+        }
+    }
+
+    function walletOfOwner(address ownerAddr) public view returns (uint256[] memory) {
+        uint256 balance = balanceOf(ownerAddr);
+        uint256[] memory tokens = new uint256[](balance);
+        uint256 tokenId;
+        uint256 found;
+
+        while (found < balance) {
+            if (_exists(tokenId) && ownerOf(tokenId) == ownerAddr) {
+                tokens[found] = tokenId;
+                found++;
+            }
+            tokenId++;
+        }
+
+        return tokens;
     }
 }
