67bd0c964cc1244c5ce2e14a5149ac7dad8474a1
moonwalker <m00n3r@protonmail.com>
2022-02-23 14:22:29 +0200

    Add last release

--- scripts/setupRelease.ts
+++ scripts/setupRelease.ts
@@ -1,13 +1,14 @@
 import hre from "hardhat";
 import { time } from "../utils";
 
-const start = time.now() - time.minutes(50 * 15);
-// const start = time.timestamp("2022-02-04T19:00:00");
+// const start = time.now() - time.minutes(50 * 15);
+const start = time.timestamp("2022-02-23T17:00:00");
 
-const end = start + time.minutes(500 * 15);
+const end = start + time.minutes(500 * 10);
 const releaseAmount = 500;
 
 async function main() {
+  console.log(`start=${start}, end=${end}, amount=${releaseAmount}`)
   await hre.run("setupRelease", { start, end, amount: releaseAmount });
 }
 
